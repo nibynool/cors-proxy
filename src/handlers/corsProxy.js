e10ac5d398eac78983273897197cbdeba8dc39f3
Michael Lambert <michael@alphageek.com.au>
2021-05-16 11:01:58 +1000

    :bug: Fix error handling

--- src/handlers/corsProxy.js
+++ src/handlers/corsProxy.js
@@ -14,27 +14,20 @@ const fetch = require('node-fetch');
 module.exports.handler = async (event) => {
     return new Promise(async (resolve, reject) => {
         let params = event.queryStringParameters;
-        let authorisedHostsRegEx = new RegExp(".*\\." + process.env.AUTHORISED_HOSTS);
-        let { Host, host, Origin, origin, ...headers } = event.headers;
 
         console.log(event);
         console.log(`Got request with params:`, params);
 
-        if (!host.match(authorisedHostsRegEx)) {
-            const errorResponse = {
-                statusCode: 401,
-                body: "Unauthorised origin host (" + host + " was found, expected " + authorisedHostsRegEx.toString() + ")",
-            };
-            reject(Error(errorResponse));
+        let authorisedOriginsRegEx = new RegExp("^https?://.*\\." + process.env.AUTHORISED_HOSTS);
+        let { Host, host, Origin, origin, ...headers } = event.headers;
+
+        if (!origin.match(authorisedOriginsRegEx)) {
+            reject(Error("Unauthorised origin (" + origin + " was found, expected " + authorisedOriginsRegEx.toString() + ")"));
             return;
         }
 
         if (!params.url) {
-            const errorResponse = {
-                statusCode: 400,
-                body: "Unable get url from 'url' query parameter",
-            };
-            reject(Error(errorResponse));
+            reject(Error("Unable get url from 'url' query parameter"));
             return;
         }
 
@@ -59,8 +52,9 @@ module.exports.handler = async (event) => {
             let proxyResponse = {
                 statusCode: res.status,
                 headers: {
-                    'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+                    'Access-Control-Allow-Origin': origin, // Required for CORS support to work
                     'Access-Control-Allow-Credentials': true, // Required for cookies, authorization headers with HTTPS
+                    'Vary': 'Origin',
                     'content-type': res.headers['content-type'],
                 },
             };
